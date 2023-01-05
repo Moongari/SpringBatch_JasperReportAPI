30bf8d874e18fd29e8ac8bf9e509f51d8bd28284
Moustafa moungari <moungari.moustafa@gmail.com>
2023-01-05 18:11:31 +0100

    partie angular first commmit

---
+++ frontPersonn_angularApi/angularPersonn/traitemnt-batch-angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StartbatchComponent } from './components/startbatch/startbatch.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StartbatchComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
